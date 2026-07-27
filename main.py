7864ab47c20f71078d776d39612cd8ed3ebf596f
Flavio Amurrio <25621374+FlavioAmurrioCS@users.noreply.github.com>
2026-07-27 03:21:19 -0400

    Checkpoint

--- main.py
+++ main.py
@@ -10,8 +10,11 @@
 import json
 import os
 import sys
-from fastapi import FastAPI, Request
+
+import anyio
 import httpx
+from fastapi import FastAPI
+from fastapi import Request
 from fastapi import Response
 
 app = FastAPI()
@@ -20,15 +23,16 @@ client = httpx.AsyncClient(base_url="https://mise-versions.jdx.dev")
 
 PATH = "/{file_path:path}"
 
+
 @app.post(PATH)
 @app.put(PATH)
 @app.delete(PATH)
-async def read_rest_of_path(file_path: str, request: Request):
+async def read_rest_of_path1(file_path: str, request: Request) -> Response:
     response = await client.request(
-            request.method,
-            file_path,
-            headers=[(k,v) for k,v in request.headers.raw if k not in (b"host",)],
-            content=await request.body()
+        request.method,
+        file_path,
+        headers=[(k, v) for k, v in request.headers.raw if k != b"host"],
+        content=await request.body(),
     )
     return Response(
         content=response.content,
@@ -36,20 +40,27 @@ async def read_rest_of_path(file_path: str, request: Request):
         headers=dict(response.headers),
     )
 
+
 @app.get(PATH)
-async def read_rest_of_path(file_path: str, request: Request):
+async def read_rest_of_path(file_path: str, request: Request) -> Response:
     path = file_path.rstrip("/")
     os.makedirs(os.path.dirname(path), exist_ok=True)
     meta_file = path + ".meta"
-    old_headers: dict[str,str] = {}
-    if os.path.exists(meta_file):
-        with open(meta_file) as f:
-            old_headers = json.load(f)
+    old_headers: dict[str, str] = {}
+    if await anyio.Path(meta_file).exists():
+        async with await anyio.open_file(meta_file, "r") as f:
+            old_headers = json.loads(await f.read())
     response = await client.request(
         request.method,
         file_path,
-        headers=[(k,v) for k,v in request.headers.raw if k not in (b"host",b"accept-encoding")] + [(b"if-none-match", old_headers.get("etag").encode() if old_headers and "etag" in old_headers else b"")],
-        content=await request.body()
+        headers=[(k, v) for k, v in request.headers.raw if k not in (b"host", b"accept-encoding")]
+        + [
+            (
+                b"if-none-match",
+                old_headers.get("etag").encode() if old_headers and "etag" in old_headers else b"",
+            )
+        ],
+        content=await request.body(),
     )
 
     content = response.content
@@ -59,28 +70,29 @@ async def read_rest_of_path(file_path: str, request: Request):
     response_headers.pop("cf-ray", None)
 
     if response.is_error:
-        if os.path.exists(path):
+        if await anyio.Path(path).exists():
             print(f"There was an error for {path} but using cached version", file=sys.stderr)
             status_code = 200
             response_headers = old_headers
-            with open(path, "rb") as f:
-                content = f.read()
+            async with await anyio.open_file(path, "rb") as f:
+                content = await f.read()
         return Response(
-                content=content,
-                status_code=status_code,
-                headers=response_headers,
-            )
+            content=content,
+            status_code=status_code,
+            headers=response_headers,
+        )
 
-    with open(meta_file, "w") as f:
+    # with open(meta_file, "w") as f:
+    async with await anyio.open_file(meta_file, "w") as f:
         json.dump(response_headers, f, indent=4, sort_keys=True)
-    if response.status_code == 304:
+    if response.status_code == 304:  # noqa: PLR2004
         print(f"Cache hit for {path}", file=sys.stderr)
         status_code = 200
-        with open(path, "rb") as f:
-            content = f.read()
+        async with await anyio.open_file(path, "rb") as f:
+            content = await f.read()
     else:
-        with open(path, "wb") as f:
-            f.write(content)
+        async with await anyio.open_file(path, "wb") as f:
+            await f.write(content)
 
     return Response(
         content=content,
@@ -88,6 +100,8 @@ async def read_rest_of_path(file_path: str, request: Request):
         headers=response_headers,
     )
 
+
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
